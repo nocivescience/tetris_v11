06907f2976e7c165818833f2ce39b60e2a31c1b9
noceive <nocivescience@gmail.com>
2022-10-19 02:12:54 -0300

    hasta acacon mi codigo

--- script.js
+++ script.js
@@ -38,27 +38,28 @@ const colors=[
     'teal',
     'mediumseagreen',
     'yellow',
+    'gold',
 ]
 const colorBlack='black';
 const scale=32;
-const cols=20;
-const rows=10;
+const cols=10;
+const rows=20;
 class Piece{
-    constructor(shape,ctx){
+    constructor(shape,color,ctx){
         this.shape=shape;
+        this.color=color
         this.ctx=ctx;
-        this.y=2*scale;
-        this.x=Math.floor(rows/2)*scale;
+        this.y=1*1;
+        this.x=Math.floor(rows/2)*1;
     }
     renderPiece(){
-        const randColor=Math.floor(Math.random()*colors.length)
         this.shape.map((row,i)=>{
             row.map((cell,j)=>{
                 if(cell!=0){
-                    this.ctx.fillStyle=colors[randColor];
-                    this.ctx.fillRect(this.x+j*scale,this.y+i*scale,scale,scale);
-                    this.ctx.strokeStyle='black';
-                    this.ctx.strokeRect(this.x+j*scale,this.y+i*scale,scale,scale)
+                    this.ctx.fillStyle=this.color;
+                    this.ctx.fillRect(this.x+j,this.y+i,1,1);
+                    this.ctx.strokeStyle=colorBlack;
+                    this.ctx.strokeRect(this.x+j,this.y+i,1,1)
                 }
             })
         })
@@ -72,38 +73,86 @@ class GameModel{
     }
     makeStartingGrid(){
         let grid=[];
-        for(let i=0;i<cols;i++){
+        for(let i=0;i<rows;i++){
             grid[i]=[];
-            for(let j=0;j<rows;j++){
-                grid[i][j]=0
+            for(let j=0;j<cols;j++){
+                grid[i][j]=0;
             }
         }
         return grid;
+    }
+    collision(x,y,candidate=null){
+        const shape=candidate||this.fallingPiece.shape;
+        const n=shape.length;
+        for(let i=0;i<n;i++){
+            for(let j=0;j<n;j++){
+                if(shape[i][j]!==0){
+                    let p=x+j;
+                    let q=i+y;
+                    if(p>=0&&p<cols&&q<rows){
+                        if(this.grid[q][p]>0){
+                            return true
+                        }
+                    }else{
+                        return true
+                    }
+                }
+            }
+        }
+        return false
     }
     renderGameState(){
         for(let i=0;i<this.grid.length;i++){
             for(let j=0;j<this.grid[i].length;j++){
                 let cell=this.grid[i][j];
+                this.ctx.fillStyle='black';
+                this.ctx.fillRect(j,i,1,1)
             }
         }
         if(this.fallingPiece!==null){
             this.fallingPiece.renderPiece();
-            setTimeout(()=>{
-                this.fallingPiece=null
-                console.log('!listo')
-            },2000)
         }
     }
     moveDown(){
         if(this.fallingPiece===null){
-            console.log('toto')
+            this.renderGameState();
+        }else if(this.collision(this.fallingPiece.x,this.fallingPiece.y+1)){
+            const shape=this.fallingPiece.shape;
+            const x=this.fallingPiece.x;
+            const y=this.fallingPiece.y;
+            shape.map((row,i)=>{
+                row.map((cell,j)=>{
+                    let p=x+j;
+                    let q=i+y;
+                    if(p>=0&&p<cols&&q<rows&&cell>0){
+                        this.grid[q][p]=shape[i][j]
+                    }
+                })
+            })
+            if(this.fallingPiece.y===0){
+                alert('are you stupid');
+                this.grid=this.makeStartingGrid()
+            }
+            this.fallingPiece=null;
+        }else{
+            this.fallingPiece.y+=1
         }
+        this.renderGameState()
     }
 }
 const model= new GameModel(ctx);
-const piece=new Piece(shapes[0],ctx)
-if(model.fallingPiece===null){
-    model.fallingPiece=piece
-    model.moveDown()
+ctx.scale(scale,scale)
+function newGameState(){
+    if(model.fallingPiece===null){
+        const rand=Math.floor(Math.random()*shapes.length)
+        const piece=new Piece(shapes[rand],colors[rand],ctx)
+        model.fallingPiece=piece;
+        model.moveDown()
+    }else{
+        model.moveDown()
+    }
+
 }
-model.renderGameState();
+setInterval(()=>{
+    newGameState()
+},1000)
